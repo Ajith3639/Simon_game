5bd1452005378301b52cee89b20f3e4d5ff170a5
Ajith <sadanandanajith@gmail.com>
2021-06-03 08:05:27 +0530

    added sound and animations to button

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var buttonColors=["red","blue","yellow","green"];
 var gamePattern=[];
-
+var userClickedPattern=[];
+$(".btn").click(function(){
+   var userChoosenColor=$(this).attr("id");
+   
+   playSound(userChoosenColor);
+   userClickedPattern.push(userChoosenColor);
+   console.log(userClickedPattern);
+});
 function nextSequence(){
     var randomColor= buttonColors[Math.floor(Math.random()*4)];
     gamePattern.push(randomColor);
     $("#"+randomColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio=new Audio("sounds/"+randomColor+".mp3");
+    audio.play();
+    playSound(randomColor);
+}
+function playSound(name){
+    var audio=new Audio("sounds/"+name+".mp3");
     audio.play();
 }
 
